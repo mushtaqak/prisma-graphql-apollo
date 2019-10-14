c716808b7859f7b66d12333d89c02c4aab6fe6ec
Mushtaq <mushtaak@gmail.com>
2019-10-14 16:02:46 +0500

    Implement fragments to fetch equipmentClasses relation

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { prisma } from "./generated/prisma-client";
 import datamodelInfo from "./generated/nexus-prisma";
 import * as path from "path";
-import {  objectType, stringArg } from "nexus";
+import { objectType, stringArg } from "nexus";
 import { prismaObjectType, makePrismaSchema } from "nexus-prisma";
 import { GraphQLServer } from "graphql-yoga";
 
@@ -18,21 +18,52 @@ const SensorType = objectType({
   }
 });
 
+const sensorsQueryType = {
+  type: "Sensor",
+  resolve: async (_, args, ctx) => {
+    // Attach equipmentClasses in query output
+    const equipmentClassesFragment = `
+      fragment SensorWithEquipmentClasses on Equipment {
+        id
+        code
+        name
+        equipmentClasses
+         {
+          id
+          code
+          name
+        }
+      }
+      `;
+    return await ctx.prisma
+      .equipments({
+        where: { equipmentClasses_some: { code: "sensor" } }
+      })
+      .$fragment(equipmentClassesFragment);
+  }
+};
+
+const createSensorType = {
+  type: "Sensor",
+  args: {
+    name: stringArg(),
+    code: stringArg()
+    // TODO: attach equipmentClasses
+  },
+  resolve: (_, { name, code }, ctx) =>
+    ctx.prisma.createEquipment({
+      code,
+      name
+    })
+};
+
 // Customize the "Query" building block
 const Query = prismaObjectType({
   name: "Query",
   definition(t) {
     t.prismaFields(["*"]);
     // Add custom sensor query
-    t.list.field("sensors", {
-      type: "Sensor",
-      resolve: async (_, args, ctx) => {
-        // TODO: attach equipmentClasses in query output
-        return await ctx.prisma.equipments({
-          where: { equipmentClasses_some: { code: "sensor" } }
-        });
-      }
-    });
+    t.list.field("sensors", sensorsQueryType);
   }
 });
 
@@ -41,19 +72,7 @@ const Mutation = prismaObjectType({
   name: "Mutation",
   definition(t) {
     t.prismaFields(["*"]);
-    t.field("createSensor", {
-      type: "Sensor",
-      args: {
-        name: stringArg(),
-        code: stringArg(),
-        // TODO: attach equipmentClasses
-      },
-      resolve: (_, { name, code }, ctx) =>
-        ctx.prisma.createEquipment({
-          code,
-          name
-        })
-    });
+    t.field("createSensor", createSensorType);
   }
 });
 
